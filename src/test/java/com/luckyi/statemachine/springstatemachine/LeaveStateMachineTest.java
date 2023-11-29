7ad8e6470339cfad1b1ed634a879bf5fc11b7fda
luckyi <974181836@qq.com>
2023-11-29 17:33:32 +0800

    :rocket: test：修改测试用例

--- src/test/java/com/luckyi/statemachine/springstatemachine/LeaveStateMachineTest.java
+++ src/test/java/com/luckyi/statemachine/springstatemachine/LeaveStateMachineTest.java
@@ -15,8 +15,8 @@ import org.springframework.statemachine.state.State;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static com.luckyi.statemachine.domain.Event.*;
-import static com.luckyi.statemachine.domain.LeaveStatusEnum.*;
+import static com.luckyi.statemachine.domain.Event.EMPLOYEE_SUBMIT;
+import static com.luckyi.statemachine.domain.LeaveStatusEnum.LEAVE_SUBMIT;
 import static com.luckyi.statemachine.domain.StateMachineConstant.STATE_MACHINE_CONTEXT;
 
 @SpringBootTest
@@ -27,15 +27,17 @@ public class LeaveStateMachineTest {
 
     @Test
     public void employSubmitRequest(){
-        leaveStateMachine.startReactively();
-        LeaveContext leaveContext = new LeaveContext(0, "");
-        Message<Event> message = MessageBuilder.withPayload(DIRECT_LEADER_AUDIT).setHeader(STATE_MACHINE_CONTEXT,leaveContext).build();
-        Flux<StateMachineEventResult<LeaveStatusEnum, Event>> resultFlux = leaveStateMachine.sendEvent(Mono.just(message));
-        resultFlux.subscribe(result -> {
-            State<LeaveStatusEnum, Event> state = result.getRegion().getState();
-            LeaveStatusEnum status = state.getId();
-            Assertions.assertEquals(LEAVE_SUBMIT,status);
-        });
+        Mono<Void> started = leaveStateMachine.startReactively();
+        started.doOnSuccess(s -> {
+            LeaveContext leaveContext = new LeaveContext(0, "");
+            Message<Event> message = MessageBuilder.withPayload(EMPLOYEE_SUBMIT).setHeader(STATE_MACHINE_CONTEXT,leaveContext).build();
+            Flux<StateMachineEventResult<LeaveStatusEnum, Event>> resultFlux = leaveStateMachine.sendEvent(Mono.just(message));
+            resultFlux.doOnComplete(() -> System.out.println("message has sent")).doOnNext(flux -> {
+                State<LeaveStatusEnum, Event> state = flux.getRegion().getState();
+                LeaveStatusEnum status = state.getId();
+                Assertions.assertEquals(LEAVE_SUBMIT,status);
+            }).subscribe();
+        }).subscribe();
     }
 
 }
